39382c069c4160254f66ed0cee55795ab87ee465
SeaPickle <uraniumpjs@gmail.com>
2022-10-22 14:01:52 -0600

    Added player direction transitions

--- Player.cpp
+++ Player.cpp
@@ -4,6 +4,7 @@ bool Player::initialize(){
 	if (!tex.loadFromFile("images/player.png")) return false;
 	if(!t_sword.loadFromFile("images/sword.png")) return false;
 	sprite.setTexture(tex);
+	sprite.setTextureRect(sf::IntRect(0,0,20,20));
 	up = false;
 	down = false;
 	left = false;
@@ -34,17 +35,22 @@ void Player::update(sf::Time dt, std::vector<sf::FloatRect>& hitboxes, sf::Vecto
 		if (!checkCollisions(vec.x+PLAYER_COLLISION_OFFSET, vec.y - PLAYER_SPEED * dt.asSeconds(), hitboxes)
 		&& !checkCollisions(vec.x+PLAYER_WIDTH-PLAYER_COLLISION_OFFSET, (vec.y - PLAYER_SPEED * dt.asSeconds()), hitboxes))
 			vec.y -= PLAYER_SPEED * dt.asSeconds();
+
+			sprite.setTextureRect(sf::IntRect(0, 0, 20, 20));
 	}
 	else if (left) {
 		if (!checkCollisions(vec.x - (PLAYER_SPEED * dt.asSeconds()), vec.y, hitboxes)&&
 			!checkCollisions(vec.x - (PLAYER_SPEED * dt.asSeconds()), vec.y+PLAYER_HEIGHT-PLAYER_COLLISION_OFFSET, hitboxes)) {
 			vec.x -= PLAYER_SPEED * dt.asSeconds();
+
+			sprite.setTextureRect(sf::IntRect(PLAYER_WIDTH*2, 0, 20, 20));
 		}
 	}
 	else if (down) {
 		if (!checkCollisions(vec.x, vec.y + (PLAYER_SPEED * dt.asSeconds() + PLAYER_HEIGHT), hitboxes)&&
 			!checkCollisions(vec.x+PLAYER_HEIGHT, vec.y + (PLAYER_SPEED * dt.asSeconds() + PLAYER_HEIGHT), hitboxes)) {
 			vec.y += PLAYER_SPEED * dt.asSeconds();
+			sprite.setTextureRect(sf::IntRect(PLAYER_WIDTH, 0, 20, 20));
 		}
 
 	}
@@ -52,6 +58,7 @@ void Player::update(sf::Time dt, std::vector<sf::FloatRect>& hitboxes, sf::Vecto
 		if (!checkCollisions(vec.x + (PLAYER_SPEED * dt.asSeconds()+PLAYER_WIDTH), vec.y, hitboxes)&&
 			!checkCollisions(vec.x + (PLAYER_SPEED * dt.asSeconds() + PLAYER_WIDTH), vec.y+PLAYER_HEIGHT, hitboxes)) {
 			vec.x += PLAYER_SPEED * dt.asSeconds();
+			sprite.setTextureRect(sf::IntRect(PLAYER_WIDTH*3, 0, 20, 20));
 		}
 	}
 	sprite.setPosition(vec);
